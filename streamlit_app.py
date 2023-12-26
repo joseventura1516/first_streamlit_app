fd8d18b1bd0434552d4664e96de24440aa5e25f5
joseventura1516 <66712439+joseventura1516@users.noreply.github.com>
2023-12-26 16:45:50 -0500

    Update streamlit_app.py
    texto

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
-import streamlit
+ 3 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 "Breakfast Menu"
+Omega 3 and Blueberry Gatmeal
+Kae, Spinach & Rocket Smoothie
+Hard-Bolied Free-Range Egg
