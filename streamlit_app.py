abf46c49eb09739561199fbfdf7aeb2263fb2198
joseventura1516 <66712439+joseventura1516@users.noreply.github.com>
2023-12-26 16:51:50 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 import streamlit
 
-streamlit.title('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-streamlit.header("Breakfast Menu")
+streamlit.title('Breakfast Menu')
+streamlit.header("Breakfast Favorite")
 streamlit.text("🥣 Omega 3 and Blueberry Gatmeal")
 streamlit.text("🥗 Kae, Spinach & Rocket Smoothie")
 streamlit.text("🐔 Hard-Bolied Free-Range Egg")
 streamlit.text("🥑🍞 Avocado Toast")
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
