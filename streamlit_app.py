04058ca21ad0e03e57e369b1c5d72a61352f85c3
joseventura1516 <66712439+joseventura1516@users.noreply.github.com>
2023-12-26 16:50:53 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.header("Breakfast Menu")
 streamlit.text("🥣 Omega 3 and Blueberry Gatmeal")
 streamlit.text("🥗 Kae, Spinach & Rocket Smoothie")
